5998d9c2f24adc8d500f21da3aa9dc69d247f423
sariswis <ss.cardenas@uniandes.edu.co>
2025-09-10 19:00:32 -0500

    delete: internal files

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,14 +2,9 @@
 
 echo "[*] Installing Python dependencies..."
 pip install -r requirements.txt
-pip install -r sidecar-requirements.txt
-pip install -r ui-requirements.txt
 
 echo "[*] Building Docker images..."
 docker build . -f alpespartners.Dockerfile -t alpespartners/flask
-docker build . -f adaptador.Dockerfile -t alpespartners/adaptador
-docker build . -f notificacion.Dockerfile -t alpespartners/notificacion
-docker build . -f ui.Dockerfile -t alpespartners/ui
 
 echo "[*] Pulling docker-compose dependencies..."
 docker-compose pull
