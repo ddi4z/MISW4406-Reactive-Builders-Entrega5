fb6d031de7790581664ba6f1b99059abfbd1804a
sariswis <ss.cardenas@uniandes.edu.co>
2025-09-21 16:03:38 -0500

    add: api

--- src/bff_web/api/v1/mutaciones.py
+++ src/bff_web/api/v1/mutaciones.py
@@ -1,22 +1,39 @@
+import uuid
 import strawberry
-from .esquemas import CrearAsociacionInput, Respuesta, crear_asociacion
+from bff_web.despachadores import Despachador
+from .esquemas import CrearAsociacionInput, Respuesta
+
+import utils
 
 @strawberry.type
 class Mutation:
     @strawberry.mutation
-    def crearAsociacion(self, input: CrearAsociacionInput) -> Respuesta:
-        payload = {
-            "id_marca": input.id_marca,
-            "id_socio": input.id_socio,
-            "tipo": input.tipo,
-            "descripcion": input.descripcion,
-            "vigencia": (
-                {
-                    "fecha_inicio": input.vigencia.fecha_inicio,
-                    "fecha_fin": input.vigencia.fecha_fin
-                } if input.vigencia else None
-            )
-        }
-        payload = {k: v for k, v in payload.items() if v is not None}
-        return crear_asociacion(payload)
+    def crearAsociacion(self, input: CrearAsociacionInput, id_usuario: str = "default_user", id_correlacion: str = str(uuid.uuid4())) -> Respuesta:
+        payload = dict(
+            id_usuario=id_usuario,
+            id_correlacion=id_correlacion,
+            id_marca=input.id_marca,
+            id_socio=input.id_socio,
+            tipo=input.tipo,
+            descripcion=input.descripcion,
+            fecha_inicio=input.vigencia.fecha_inicio if input.vigencia else None,
+            fecha_fin=input.vigencia.fecha_fin if input.vigencia else None,
+        )
+        
+        comando = dict(
+            id = str(uuid.uuid4()),
+            time=utils.time_millis(),
+            specversion="v1",
+            type="CrearAsociacionEstrategica",
+            ingestion=utils.time_millis(),
+            datacontenttype="AVRO",
+            service_name="BFF Web",
+            data=payload,
+        )
+        
+        despachador = Despachador()
+        input.context["background_tasks"].add_task(despachador.publicar_mensaje, comando, "comando-crear-asociacion-estrategica", "public/default/comando-crear-asociacion-estrategica")
 
+        return Respuesta(mensaje="Procesando Mensaje", codigo=203, idSolicitud=id_correlacion)
+
+    
